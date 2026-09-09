f241282491070021720066f9f28882886b1bdd22
Thoroughly-end <allenkuo0818@gmail.com>
2026-09-09 19:29:26 +0800

    add widget

---
+++ ScheduleWidget/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  ScheduleWidget
+//
+//  Created by 郭家駿 on 2026/9/9.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource { "Configuration" }
+    static var description: IntentDescription { "This is an example widget." }
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
---
+++ ScheduleWidget/ScheduleWidgetBundle.swift
@@ -0,0 +1,18 @@
+//
+//  ScheduleWidgetBundle.swift
+//  ScheduleWidget
+//
+//  Created by 郭家駿 on 2026/9/9.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct ScheduleWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        ScheduleWidget()
+        ScheduleWidgetControl()
+        ScheduleWidgetLiveActivity()
+    }
+}
